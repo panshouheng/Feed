9d89df59a6b2a7bdacb783a68bfddf1016e96eb9
psh ICN <apple@appledemacbook-pro.local>
2016-09-02 15:17:21 +0800

    Initial Commit

---
+++ Feed/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Feed
+//
+//  Created by apple on 16/9/2.
+//  Copyright © 2016年 psh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Feed/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Feed
+//
+//  Created by apple on 16/9/2.
+//  Copyright © 2016年 psh. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
